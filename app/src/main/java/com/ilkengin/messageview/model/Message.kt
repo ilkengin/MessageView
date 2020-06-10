4ff85a0441ad552d590db46f95d7db4bdb2e0b64
Ilkan Engin <ilkan.engin@siemens.com>
2020-06-10 21:49:34 +0300

    added MessageDeliveryStatus in Message

--- app/src/main/java/com/ilkengin/messageview/model/Message.kt
+++ app/src/main/java/com/ilkengin/messageview/model/Message.kt
@@ -3,12 +3,14 @@ package com.ilkengin.messageview.model
 class Message {
     var id: Int = 0
     var type: MessageType = MessageType.RECEIEVED
+    var status: MessageDeliveryStatus = MessageDeliveryStatus.SENT
     var senderName: String = ""
     var text: String = ""
 
-    constructor(_id: Int, _senderName: String, _type: MessageType, _text: String) {
+    constructor(_id: Int, _senderName: String, _type: MessageType, _status: MessageDeliveryStatus, _text: String) {
         this.id = _id
         this.type = _type
+        this.status = _status
         this.senderName = _senderName
         this.text = _text
     }
