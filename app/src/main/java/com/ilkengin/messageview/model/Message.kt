28967ce300100ed16addcee605b344736309783b
Ilkan Engin <ilkan.engin@siemens.com>
2020-06-12 22:09:17 +0300

    Added a new constructor in Message to prevent unnecessary field settings for Sent and Received messages

--- app/src/main/java/com/ilkengin/messageview/model/Message.kt
+++ app/src/main/java/com/ilkengin/messageview/model/Message.kt
@@ -2,16 +2,25 @@ package com.ilkengin.messageview.model
 
 class Message {
     var id: Int = 0
-    var type: MessageType = MessageType.RECEIEVED
-    var status: MessageDeliveryStatus = MessageDeliveryStatus.SENT
-    var senderName: String = ""
     var text: String = ""
+    var from: String = ""
+    var type: MessageType = MessageType.RECEIEVED
+    var status: MessageDeliveryStatus = MessageDeliveryStatus.NA
 
-    constructor(_id: Int, _senderName: String, _type: MessageType, _status: MessageDeliveryStatus, _text: String) {
+
+    constructor(_id: Int, _text: String, _status: MessageDeliveryStatus = MessageDeliveryStatus.SENDING) {
         this.id = _id
-        this.type = _type
-        this.status = _status
-        this.senderName = _senderName
         this.text = _text
+        this.from = ""
+        this.type = MessageType.SENT
+        this.status = _status
+    }
+
+    constructor(_id: Int, _text: String, _from: String) {
+        this.id = _id
+        this.text = _text
+        this.from = _from
+        this.type = MessageType.RECEIEVED
+        this.status = MessageDeliveryStatus.NA
     }
 }
